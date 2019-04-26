84b7b8ba363decb64a51b41b6c3486b6f9b561ad
redpanda1234 <fkobayashi@hmc.edu>
2019-04-26 10:37:00 -0700

    continue working on hom constructor

--- lefschetz/lefschetz.py
+++ lefschetz/lefschetz.py
@@ -25,7 +25,7 @@ class SimplicialComplex:
     def __repr__(self):
         out_str = f"Simplicial Complex of max dimension {self.n}." + "\n---------------------------------------------\n" + "Subsimplices:\n"
         for i in range(self.n+1):
-            out_str += f"    i={i}:"
+            out_str += f"    n={i}:"
             sorted_simps = sorted([sorted(list(simp)) for simp in list(self.sdict[i])])
             for i, simp in enumerate(sorted_simps):
                 if i == 0:
@@ -100,7 +100,11 @@ class SimplicialComplex:
 
             # Intersect all the parts from self
             for i in range(o_n+1):
-                new_sdict[i] |= self.sdict[i]
+                new_sdict[i] &= self.sdict[i]
+
+                # Delete empty sets
+                if not new_sdict[i]:
+                    del new_sdict[i]
 
             return SimplicialComplex(new_sdict)
 
@@ -110,7 +114,11 @@ class SimplicialComplex:
 
             # Union in all the parts from self
             for i in range(s_n+1):
-                new_sdict[i] |= other.sdict[i]
+                new_sdict[i] &= other.sdict[i]
+
+                # Delete empty sets
+                if not new_sdict[i]:
+                    del new_sdict[i]
 
             return SimplicialComplex(new_sdict)
 
@@ -158,6 +166,151 @@ class SimplicialComplex:
         return self.union(other)
 
 
+    def subtract(self, other):
+        """
+        todo
+        """
+        return
+
+# We need these things to work without having to go through the
+# overhead of instantiating a class.
+# TODO: refactor to define methods in terms of these, perhaps?
+def is_valid(sdict):
+    """
+    check if self is a valid simplicial complex
+    """
+    bad_simps = {}
+       # Check that all of the simplices are actually valid by
+    for i in range(0, len(sdict.keys()), -1):
+        for simplex in sdict[i]:
+            is_valid, bad_simps = check_faces(sdict, simplex, bad_simps)
+            if not is_valid:
+                return False
+    return True
+
+def check_faces(sdict, simplex, bad_simps):
+    """
+    Inputs:
+    -------
+    simplex: k-simplex (represented as a set of ints)
+    """
+    # Get the dimension of the simplex
+    k = len(simplex)
+
+    # Need to check all the way down to 0 simplices. k-simplices
+    # aren't necessary, since this will only be called by
+    # iterating over all k-simplices.
+    for r in range(0, k, -1):
+        r_combo_iterator = chain.from_iterable(combinations(simplex, r))
+        for r_combo in r_subset_iterator:
+            # chain.from_iterable spits out a tuple, not a set
+            r_subset = set(r_combo)
+
+            # Is this really a valid r simplex?
+            if r_subset in sdict[r]:
+                pass
+            elif r_subset in bad_simps:
+                return False, bad_simps
+            else:
+                return False, bad_simps | {simplex, r_subset}
+    return True, bad_simps
+
+def apply_hom(f, K):
+    """
+    f: a dictionary associating verts in K to verts in the image of f
+
+    Apply the simplicial map f over the simplex dict K
+    """
+    # Max value of n we can iterate to
+    max_iter = len(K)+1
+    out_sdict = {frozenset() for i in range(max_iter)}
+    # Iterate through dimensions
+    for n in range(max_iter):
+        for knsimp in K[n]:
+            # Image simplex
+            imsimp = {}
+            # Iterate through this n simplex in k
+            for vert in knsimp:
+                imsimp.add(f[vert])
+            # Image of this n simplex need not be an n simplex
+            out_sdict[len(imsimp)] |= frozenset(imsimp)
+        # Currently in the middle of trying to write the logic to
+        # merge all the images of the simplices (as determined by
+        # verts) together
+    # This needs testing!
+    return out_sdict
+
+
+def Hom_smap(K,L):
+    """
+    Homset of simplicial maps.
+
+    K, L are simplicial complexes.
+
+    generate all simplicial maps between the two simplicial complexes
+    provided.
+    """
+    kverts, lverts = k.sdict[0], l.sdict[0]
+
+    # Store previous vertex configurations that we've already tried in
+    # a memo so that maybe we won't have n! runtime?
+    memo = {}
+
+
+    # Initialize output set of simplicial mpas
+    all_smaps = {}
+
+    # Current simplicial map (represented as a dictionary)
+    smap = {}
+
+    def smap_constructor(smap, kvset, lvset):
+        """
+        kvset, lvset --- same as kverts, lverts. Just using a
+        different name b/c I forgot how global vars work in python and
+        want to avoid naming conflicts.
+        """
+        # Base case: all points in the domain have been assigned.
+        if not(kverts):
+            return smap
+        for bad_map in memo:
+            if dict_subset(bad_map, smap):
+                return False
+
+        else:
+            for lv in lvset:
+                new_smap = smap.copy()
+
+                # pop a vertex out
+                new_kvset = kvset.copy()
+                this_k = new_kvset.pop()
+
+                # Randomly assign an image
+                new_smap[this_k] = lv
+
+                new_complex = apply_hom(new_smap, kvset)
+                if is_valid(new_complex):
+                    # TODO: Maybe figure out a way to lazy-evaluate
+                    # these things. Perhaps an iterator could be
+                    # useful?
+
+                    return smap_constructor(smap_copy, new_kvset, lvset)
+                else:
+                    memo.add(smap)
+                    return False
+
+    return
+
+def dict_subset(d1, d2):
+    """
+    check if one of the input dictionaries is a subset of the other.
+
+    in particular, if d1 <= d2
+    """
+    return all(d1.get(k, object()) == v for (k, v) in d2.items())
+
+
+
+
 def standard_simp(verts):
     """
     For recursively constructing a standard n-simplex from a list of verts.
@@ -182,7 +335,11 @@ def standard_simp(verts):
 def test_simplex():
     sdict1 = standard_simp({1,2,3})
     sdict2 = standard_simp({2,3,4})
+    sdict3 = standard_simp({5,6,7,8})
 
-    return SimplicialComplex(sdict1) | SimplicialComplex(sdict2)
+    scomp1 = SimplicialComplex(sdict1)
+    scomp2 = SimplicialComplex(sdict2)
+    scomp3 = SimplicialComplex(sdict3)
+    return scomp1, scomp2, scomp3
 
-test = test_simplex()
+scomp1, scomp2, scomp3 = test_simplex()
